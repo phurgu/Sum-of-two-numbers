01f08a11558758ed896d6929a247a2023bf4f937
phurgu <83075757+phurgu@users.noreply.github.com>
2021-04-26 18:30:10 +0545

    Add files via upload

---
+++ Sum.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main()
+{
+    int a, b, sum;
+    printf("Enter first number:");
+    scanf("%d",&a);
+    printf("Enter second number:");
+    scanf("%d",&b);
+    sum = a + b;
+    scanf("d",&sum);
+    printf("The sum of two numbers is %d \n",sum); 
+    return 0;
+}
